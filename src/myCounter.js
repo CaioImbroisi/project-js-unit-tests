39aba4a929898595ae82a12a45256fb7eaab4756
Caio Imbroisi <caio.imbroisi@gmail.com>
2022-05-16 20:46:49 -0300

    added unit test files

---
+++ src/myCounter.js
@@ -0,0 +1,31 @@
+/* eslint-disable no-var */
+/* eslint-disable block-scoped-var */
+/* eslint-disable no-redeclare */
+/* eslint-disable vars-on-top */
+
+/*
+  A função myCounter possui dois loops aninhados que inserem valores dentro de um array.
+  Como podemos perceber, eles vão adicionando valores ao array até sua condição de parada.
+  Corrija o código abaixo para que a função retorne o array correto.
+  Atenção: não remova os `for`s da função e sim corrija os erros presentes.
+
+  Parâmetros:
+  - Nenhum.
+
+  Comportamento:
+  myCounter() // Retorna: [0, 2, 3, 1, 2, 3, 2, 2, 3, 3, 2, 3];
+*/
+
+const myCounter = () => {
+  var myArray = [];
+  for (let counterA = 0; counterA <= 3; counterA += 1) {
+    myArray.push(counterA);
+    for (let counterB = 2; counterB <= 3; counterB += 1) {
+      myArray.push(counterB);
+    }
+  }
+  return myArray;
+};
+console.log(myCounter());
+
+module.exports = myCounter;
